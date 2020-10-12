e6f0af498c2d9484eca7eedc50f13645bdf90341
Yelsaw <47115872+yelsaw@users.noreply.github.com>
2020-10-12 10:47:46 -0500

    update app/bin/run.pl w/ query.pl

--- query.sh
+++ query.sh
@@ -1,1 +1,1 @@
-docker exec -it $(docker container ls -a  -q --filter "ancestor=clearbit" --filter "status=running") bin/run.pl $1
+docker exec -it $(docker container ls -a  -q --filter "ancestor=clearbit" --filter "status=running") bin/query.pl $1
