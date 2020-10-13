023d0a89232503b5947fe4dca17217e10b70a62e
yelsaw <clearbit@yelsaw.com>
2020-10-12 23:07:15 -0500

    use volume to mount /app dir in container

--- Dockerfile
+++ Dockerfile
@@ -1,27 +1,25 @@
 FROM alpine:3.9
 
-WORKDIR /
+WORKDIR /setup
 
-COPY packages.txt cpanfile cpanfile.snapshot /
+COPY setup/* /setup/
 
-RUN set -ex && \
-    apk update && \
-    apk add --no-cache $(cat /packages.txt|grep -v ^#)
-
-RUN set -ex && \
-    cpanm -n Carton 
+RUN set -ex && apk update \
+    && apk add --no-cache $(cat /setup/packages.txt|grep -v ^#) \
+    && cpanm -n Carton
 
 ENV PERL_CARTON_PATH=/local
-ENV PERL_CARTON_CPANFILE=/cpanfile
+ENV PERL_CARTON_CPANFILE=/setup/cpanfile
 
 RUN set -ex \
-  && carton install --deployment --cpanfile=/cpanfile \
+  && carton install --deployment --cpanfile=/setup/cpanfile \
   && rm -rf /local/cache \
   && rm -rf /root/.cpanm
 
-COPY app /app
-RUN chmod +x /app/bin/query.pl
+#COPY app /app
+#RUN chmod +x /app/bin/query.pl
 
 ENV PERL5LIB=/local/lib/perl5
 
+VOLUME /app
 WORKDIR /app
--- start.sh
+++ start.sh
@@ -1,29 +1,29 @@
 #!/bin/bash
 
 set -e
-
-cb=$(docker container ls -a  -q --filter "ancestor=clearbit" --filter "status=running" 2> /dev/null)
+cb=$(./container-id running)
 im=$(docker images -q clearbit 2> /dev/null)
 
 if [ $cb ]; then
-  echo clearit is running under container id: $cb
-  echo type a search string to query and press enter. Or type x to exit
+  echo "clearbit is running under container id: $cb"
+  echo "type a search string to query and press enter. Or type x to exit"
+  echo ""
   read enter
-  if [ $enter != 'x' ]; then
-    ./query.sh $enter
+  if [ $enter == 'x' ]; then
+     exit 0
    else
-    exit 0
+    ./query.sh $enter
   fi
 else
   if [ $im ]; then
-    echo starting clearbit container
-    docker run -it -d $im
+    echo "starting clearbit container"
+    docker run -it -d clearbit
     exit 0;
   else
-  echo Building clearbit image
+  echo "Building clearbit image"
   sleep 2
   docker build -t clearbit . &&
-  echo Starting clearbit container
-  docker run -it -d --name=clearbit
+  echo "Starting clearbit container"
+  docker run -it -d -v$PWD/app:/app:ro clearbit
   fi;
 fi;
