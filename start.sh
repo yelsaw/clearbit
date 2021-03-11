5543e4105e67d67d456db40d08b6169d4468715a
Yelsaw <47115872+yelsaw@users.noreply.github.com>
2021-03-11 00:04:02 -0600

    add arg to start.sh to choose image name
    Add ability to choose an image / container name and run multiple instances.
    Usage: 
    ./start.sh some-instance-name
    ./start.sh another-instance-name

--- start.sh
+++ start.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 set -e
-cb=$(docker ps -qf name=clearbit)
-im=$(docker images -q clearbit 2> /dev/null)
+
+if [ -z $1 ]; then
+  echo "Enter a docker image name to use this script"
+  exit 0
+fi
+
+name=$1
+cb=$(docker ps -qf name=$name)
+im=$(docker images -q $name 2> /dev/null)
 
 if [ $cb ]; then
-  echo "clearbit is running under container id: $cb"
-  echo "type a search string to query and press enter. Or type x to exit"
+  echo "$name is running under container id: $cb"
+  echo "Provide arguments for ./query.sh. Or type x to exit"
   echo ""
   read enter
   if [ $enter == 'x' ]; then
@@ -16,14 +23,14 @@ if [ $cb ]; then
   fi
 else
   if [ $im ]; then
-    echo "starting clearbit container"
-    docker run -it -d --name=clearbit clearbit
+    echo "starting $name container"
+    docker run -it -d --name=$name $name
     exit 0;
   else
-  echo "Building clearbit image"
+  echo "Building $name image"
   sleep 2
-  docker build -t clearbit . &&
-  echo "Starting clearbit container"
-  docker run -it -d --name=clearbit clearbit
+  docker build -t $name . &&
+  echo "Starting $name container"
+  docker run -it -d --name=$name $name
   fi;
 fi;
