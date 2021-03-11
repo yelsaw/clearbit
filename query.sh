7068230c701824f31990cdd4552d6d4298b8e292
Yelsaw <47115872+yelsaw@users.noreply.github.com>
2021-03-11 00:15:38 -0600

    add arg to query.sh to choose container name
    Add ability to choose a container name to query.
    Usage: 
    ./query.sh some-container-name query-string
    ./query.sh another-instance-name query-string

--- query.sh
+++ query.sh
@@ -1,6 +1,18 @@
-cb=$(docker ps -qf name=clearbit)
+#!/bin/bash
+
+set -e
+
+if [ -z $1 ] && [ -z $2 ]; then
+  echo "Enter a docker container name and one arg to use this script"
+  exit 0
+fi
+
+name=$1
+query=$2
+
+cb=$(docker ps -qf name=$name)
 if [ $cb ]; then
-  docker exec -it $cb bin/query.pl $1
+  docker exec -it $cb bin/query.pl $query
 else
-  echo "container doesn't exist, run ./start.sh to start or build and start"
+  echo "container doesn't exist, run './start.sh $name' to start or build and start"
 fi
